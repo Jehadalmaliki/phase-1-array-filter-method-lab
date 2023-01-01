1ec3c58b4664e2baca1692f8f3ffc229412b6fe0
Jehad Almaliki <yourgitemail@example.com>
2023-01-01 22:49:24 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = [
+  { name: "John", age: 30 },
+  { name: "Jane", age: 25 },
+  { name: "Bob", age: 35 }
+];
+
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+
+function fuzzyMatch(drivers, searchTerm) {
+  return drivers.filter(driver => driver.startsWith(searchTerm));
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
